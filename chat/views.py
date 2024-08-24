2c502252ea5b94de91719cfdfaa91973fe58c1ae
Xiao Yav1 <1409951865@qq.com>
2024-08-24 18:52:43 +0800

    添加事务装饰器

--- chat/views.py
+++ chat/views.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from django.db import transaction
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.generics import GenericAPIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from django.http import StreamingHttpResponse
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-client = OpenAI(
+openai = OpenAI(
     api_key=os.getenv("API_KEY"),
     organization=os.getenv("ORGANIZATION"),
     project=os.getenv("PROJECT"),
@@ -23,7 +23,8 @@ client = OpenAI(
 
 
 # Create your views here.
-class ChatView(GenericAPIView):
+class ChatView(APIView):
+    @transaction.atomic
     def post(self, request):
         # 获取相关参数
         data = json.loads(request.body)
@@ -33,49 +34,63 @@ class ChatView(GenericAPIView):
         user = request.user
         fetch_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-        # api需要的参数
-        messages_list = Message.objects.filter(room=room)[:50]
-        serializer_message_list = MessageSerializer(messages_list, many=True)
+        # 获取消息上下文
+        serializer_message_list = self.get_messages(room)
         message_params = serializer_message_list.data + [{'role': 'user', 'content': content}]
 
-        #
+        # 准备 OpenAI API 请求数据
         messages = [{"role": "system", "content": "You are a helpful assistant."}] + message_params
-        completion = client.chat.completions.create(
+        # 发送请求
+        completion = openai.chat.completions.create(
             model=model,
             messages=messages,
             stream=True,
         )
 
-        def event_stream():
-            ai_content = ''
-            try:
-                for chunk in completion:
-                    delta = chunk.choices[0].delta
-                    if delta.content:
-                        ai_content += delta.content
-                    yield delta.content
-            except Exception as e:
-                yield f"data: Error: {str(e)}"
-            finally:
-                if ai_content != '':
-                    # 存当前的上下文
-                    res_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                    message_dict = {"role": 'user', "content": content, "room": room, "user": user.id, "model": model,
-                                    'date_time': fetch_time}
-                    ai_message_dict = {"role": 'assistant', "content": ai_content, "room": room, "user": user.id,
-                                       "model": model, 'date_time': res_time}
-                    cxt_list = [message_dict, ai_message_dict]
-                    message_serializer = MessageSerializer(data=cxt_list, many=True)
-                    if message_serializer.is_valid():
-                        message_serializer.save()
-                    else:
-                        print(message_serializer.errors)
-                        return Response(message_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        response = StreamingHttpResponse(event_stream(), content_type='text/event-stream')
+        response = StreamingHttpResponse(self.event_stream(completion, content, room, user, fetch_time, model),
+                                         content_type='text/event-stream')
         response['Cache-Control'] = 'no-cache'
 
         return response
+
+    def event_stream(self, completion, content, room, user, fetch_time, model):
+        ai_content = ''
+        try:
+            for chunk in completion:
+                delta = chunk.choices[0].delta
+                if delta.content:
+                    ai_content += delta.content
+                yield delta.content
+        except Exception as e:
+            yield f"data: Error: {str(e)}"
+        finally:
+            if ai_content:
+                self.save_message(content, ai_content, room, user, model, fetch_time)
+
+    def save_message(self, content, ai_content, room, user, model, fetch_time):
+        # 存当前的上下文
+        res_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        # 使用字典推导式创建消息字典
+        common_fields = {
+            "room": room,
+            "user": user.id,
+            "model": model,
+        }
+        message_dict = {**common_fields, "role": 'user', "content": content, "date_time": fetch_time}
+        ai_message_dict = {**common_fields, "role": 'assistant', "content": ai_content, "date_time": res_time}
+        cxt_list = [message_dict, ai_message_dict]
+
+        message_serializer = MessageSerializer(data=cxt_list, many=True)
+        if message_serializer.is_valid():
+            message_serializer.save()
+        else:
+            print(message_serializer.errors)
+            return Response(message_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_messages(self, room):
+        messages_list = Message.objects.filter(room=room).order_by('id')[:20].only('role', 'content')
+        serializer_message_list = MessageSerializer(messages_list, many=True)
+        return serializer_message_list
 
 
 class RoomView(ModelViewSet):
@@ -110,8 +125,6 @@ class RoomView(ModelViewSet):
 
 class MessageView(ModelViewSet):
     queryset = Message.objects.all()
+    pagination_class = None
     serializer_class = MessageSerializer
     filterset_fields = ['user', 'room']  # 过滤字段
-    search_fields = ['content']  # 搜索字段
-    ordering_fields = ['user__username', 'id']  # 允许排序的字段
-    ordering = ['id']  # 默认排序
